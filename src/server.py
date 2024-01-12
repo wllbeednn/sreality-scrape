e66a688df1b2ba1e53d7930cdf1b47fcea4f6480
Nikita Bardatskii <bardanik@fit.cvut.cz>
2024-01-12 21:54:51 +0100

    Finish making template

--- src/server.py
+++ src/server.py
@@ -1,17 +1,25 @@
 import uvicorn
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
 from typing import Annotated
+
 from db.session import Session
 from db import get_db
 from crud import get_all
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def get_flats(db: Annotated[Session, Depends(get_db)]):
-    return get_all(db=db)
+
+@app.get("/", response_class=HTMLResponse)
+async def get_flats(request: Request, db: Annotated[Session, Depends(get_db)]):
+    flats = get_all(db=db)
+    return templates.TemplateResponse(
+        request=request, name="flats.html", context={"flats": flats}
+    )
 
 
 if __name__ == "__main__":
