e2bfa5f71ac0a4456d9d517ccb7764799379755e
Nikita Bardatskii <bardanik@fit.cvut.cz>
2024-01-12 19:31:35 +0100

    Completed crawler

--- src/scraper.py
+++ src/scraper.py
@@ -1,28 +1,29 @@
+import json
+import math
+
 import scrapy
 
 
 class FlatsScraper(scrapy.Spider):
     name = 'sreality-flats-scraper'
-    start_urls = ["https://www.sreality.cz/hledani/prodej/byty"]
+    allowed_domains = ["sreality.cz"]
+
+    page = 1
+    per_page = 20
+    estates_number = 40
+    max_pages = math.ceil(estates_number / per_page)
+
+    api_endpoint = "https://www.sreality.cz/api/cs/v2/estates"
+    start_urls = [
+        f"{api_endpoint}?category_main_cb=1&category_type_cb=1&page={page}&per_page={per_page}"
+    ]
 
     def parse(self, response, kwargs=None):
-        self.logger.debug("=================STARTING CRAWLING=================")
-        print(response.body)
-        with open('output.html', 'w') as file:
-            file.write(response.body.__str__())
-
-        print(response.css("div.dir-property-list div.property"))
-
-        for flat in response.css("div.dir-property-list div.property"):
-            self.logger.debug("-------------------------------------------------")
-            print(flat)
-            self.logger.debug("-------------------------------------------------")
-            break
-        self.logger.debug("=================FINISHING CRAWLING=================")
-        #     yield {
-        #         "title": quote.xpath("span/small/text()").get(),
-        #     }
-
-        # next_page = response.css('li.next a::attr("href")').get()
-        # if next_page is not None:
-        #     yield response.follow(next_page, self.parse)
+        data = json.loads(response.text)
+        estates = data["_embedded"]["estates"]
+        for estate in estates:
+            print(estate['name'])
+        if self.page < self.max_pages:
+            self.page += 1
+            url = f"{self.api_endpoint}?category_main_cb=1&category_type_cb=1&page={self.page}&per_page={self.per_page}"
+            yield scrapy.Request(url=url, callback=self.parse)
