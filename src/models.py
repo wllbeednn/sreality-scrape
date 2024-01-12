b30a9fdb9d6cb0f038209c000ae4230a3f8625fa
Nikita Bardatskii <bardanik@fit.cvut.cz>
2024-01-12 20:57:37 +0100

    add primary key to flat as autogenerated uuid

--- src/models.py
+++ src/models.py
@@ -1,7 +1,10 @@
 from db.base_class import Base
 from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
+from uuid import uuid4
 
 
 class Flat(Base):
+    uuid = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     title = Column(String, nullable=False)
     image_url = Column(String, nullable=True)
