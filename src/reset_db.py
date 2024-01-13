161d230a1b4800f47c261df7480ea5cd0cd2cb90
Nikita Bardatskii <bardanik@fit.cvut.cz>
2024-01-13 17:40:25 +0100

    Hotfix for case when running docker compose db is not yet ready

--- src/reset_db.py
+++ src/reset_db.py
@@ -4,5 +4,15 @@ Script to reset database.
 from db import Base
 from db.session import engine
 from models import Flat
+from sqlalchemy.exc import SQLAlchemyError
+import time
+import logging
 
-Base.metadata.drop_all(engine)
+try:
+    Base.metadata.drop_all(engine)
+except SQLAlchemyError as e:
+    # probably docker container with db is not initialized, just wait a little bit
+    logging.warning(msg="Probably db is not yet initialized, sleeping for a moment.")
+
+    time.sleep(7)
+    Base.metadata.drop_all(engine)
